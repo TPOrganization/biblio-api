e913bbedf6ff43e7f354b6954cff9d6e03b5bb14
Angelique Grincourt <angelique.grnt@gmail.com>
2023-09-28 16:17:40 +0200

    fix: camelCase id entity BA-23

--- src/_controller/_database/_entity/author/author.entity.ts
+++ src/_controller/_database/_entity/author/author.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 @Entity()
 export class Author {
     @PrimaryGeneratedColumn()
-    Id: number
+    id: number
 
     @Column()
     name : string
--- src/_controller/_database/_entity/status/status.entity.ts
+++ src/_controller/_database/_entity/status/status.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 @Entity()
 export class Status {
     @PrimaryGeneratedColumn()
-    Id: number
+    id: number
 
     @Column()
     label : string
