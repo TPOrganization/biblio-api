d518ad4cc8afa8e2bc92134276f3ade73ffcfcfe
Pinvin Thomas <thomas.pinvin@digixp.fr>
2023-10-05 12:03:28 +0200

    fix: JWT init module

--- src/_controller/authentification/auth.module.ts
+++ src/_controller/authentification/auth.module.ts
@@ -3,9 +3,23 @@ import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { LocalStrategy } from './strategies/local.strategy'
 import { JwtStrategy } from './strategies/jwt.strategy'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Global()
 @Module({
+    imports: [
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: { expiresIn: `${configService.get<string>('JWT_EXPIRE_SECONDS')}s` },
+            }),
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [AuthController],
     providers: [
         AuthService, 
