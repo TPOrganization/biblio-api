713771a044e3635ff84ced3c393e7c6a58e0fd99
Angelique Grincourt <angelique.grnt@gmail.com>
2023-10-25 10:25:57 +0200

    fix: token key

--- src/_controller/authentification/auth.service.ts
+++ src/_controller/authentification/auth.service.ts
@@ -17,17 +17,17 @@ export interface SignUpData {
 @Injectable()
 export class AuthService {
     constructor(
-        private readonly _userService: UserService,
-        private readonly _jwtService: JwtService,
-        private readonly _configService: ConfigService,
-        private readonly _mailService: MailService
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService,
+        private readonly mailService: MailService
     ) { }
 
     async validateUser(login: string, password: string): Promise<any> {
         if (!login) {
             throw new UnauthorizedException()
         }
-        const user = await this._userService.findLogin(login)
+        const user = await this.userService.findLogin(login)
         if (!user || !password) {
             throw new UnauthorizedException()
         }
@@ -44,7 +44,7 @@ export class AuthService {
 
     async signIn(user: User) {
         return {
-            token: this._jwtService.sign({ user: user }),
+            accessToken: this.jwtService.sign({ user: user }),
             user: user
         }
     }
@@ -55,18 +55,18 @@ export class AuthService {
             throw new ForbiddenException()
         }
 
-        const userLogin = await this._userService.findLogin(user.email)
+        const userLogin = await this.userService.findLogin(user.email)
         if (userLogin) {
             throw new ForbiddenException()
         }
 
         const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(user.password, salt)
-        const createNewUser = this._userService._repository.create({
+        const createNewUser = this.userService._repository.create({
             ...user,
             password: hashPassword
         })
-        const result = await this._userService._repository.save(createNewUser)
+        const result = await this.userService._repository.save(createNewUser)
         delete result.password
         return result
     }
@@ -76,24 +76,24 @@ export class AuthService {
         if (!email) {
             throw new UnauthorizedException()
         }
-        const user = await this._userService.findLogin(email)
+        const user = await this.userService.findLogin(email)
         if (!user) {
             throw new UnauthorizedException()
         }
 
         delete user.password
-        const token = await this._jwtService.sign({ ...user })
-        const link = `${this._configService.get<string>('FRONT_URL')}/reset-password?token=${token}`
-        return await this._mailService.sendMail(user.email, 'reset mdp', link)
+        const token = await this.jwtService.sign({ ...user })
+        const link = `${this.configService.get<string>('FRONT_URL')}/reset-password?token=${token}`
+        return await this.mailService.sendMail(user.email, 'reset mdp', link)
     }
 
     async resetPassword(user: User, password: string, confirm: string) {
         if (password === confirm) {
             const salt = await bcrypt.genSalt(10)
             const hashNewPassword = await bcrypt.hash(password, salt)
-            const userExist = await this._userService.findOne(user.id)
+            const userExist = await this.userService.findOne(user.id)
             if (userExist) {
-                await this._userService._repository.save({
+                await this.userService._repository.save({
                     ...user,
                     password: hashNewPassword
                 })
