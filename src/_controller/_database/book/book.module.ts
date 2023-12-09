4110d41d0dcba8c65b0921d999e3d7329109c7a5
Pinvin Thomas <thomas.pinvin@digixp.fr>
2023-12-09 17:11:15 +0100

    fix: linter

--- src/_controller/_database/book/book.module.ts
+++ src/_controller/_database/book/book.module.ts
@@ -1,7 +1,5 @@
 import { Global, Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { StorageService } from 'src/_helper/storage/storage.service'
-import { CryptoService } from 'src/_helper/_crypto.service'
 import { Book } from '../_entity/book/book.entity'
 import { BookService } from './book.service'
 import { BookController } from './book.controller'
